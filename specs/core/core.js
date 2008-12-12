9e7176dc1fff2b72d2a1bc5cc7e1bf80def24121
xan <xan@4ad869c7-1038-764e-a043-2e1a7334228a>
2008-12-12 18:51:54 +0000

    начал рефакторинг core
    git-svn-id: http://repos.4ap.ru/work/ui/trunk@2903 4ad869c7-1038-764e-a043-2e1a7334228a

---
+++ specs/core/core.js
@@ -0,0 +1,251 @@
+/*
+Script: core.js
+	Examples for core.js
+*/
+describe('core', {
+    'shoule exists as singleton object into global namespace' : function () {
+        value_of(window.AP).should_not_be_undefined();
+        value_of(window.AP).should_not_be_null();
+        value_of(typeof window.AP).should_be('function');
+    },
+    
+    'should have method namespace' : function () {
+        value_of(AP.namespace).should_not_be_undefined();
+        value_of(AP.namespace).should_not_be_null();
+        value_of(typeof AP.namespace).should_be('function');
+    },
+    
+    'should allow create packages and sub-packages with namespace method' : function () {
+        AP.namespace('Test');
+        value_of(AP.Project.Test).should_not_be_undefined();
+        value_of(AP.Project.Test).should_not_be_null();
+        value_of(typeof AP.Project.Test).should_be('object');
+    
+        // test that AP.Project.Test should be empty
+        for (var prop in AP.Project.Test) {}
+        value_of(prop).should_be(undefined);
+        
+        // test sub-packages
+        AP.namespace('Test.SubPackage');
+        value_of(AP.Project.Test.SubPackage).should_not_be_undefined();
+        value_of(AP.Project.Test.SubPackage).should_not_be_null();
+        value_of(typeof AP.Project.Test.SubPackage).should_be('object');
+        // package Test should have exactly one property
+        var count = 0;
+        for (prop in AP.Project.Test) {
+            count++;
+        }
+        value_of(count).should_be(1);
+        
+        prop = null;
+
+        // test that AP.Project.Test should be empty
+        for (prop in AP.Project.Test.SubPackage) {}
+        value_of(prop).should_be_null;
+        
+        // test that AP.namespace('Test.AnotherOneSubPackage')
+        // should not override existing package
+        AP.namespace('Test.AnotherOneSubPackage');
+        count = 0;
+        for (prop in AP.Project.Test) {
+            count++;
+        }
+        value_of(count).should_be(2);
+        value_of(AP.Project.Test).should_include('AnotherOneSubPackage');
+        value_of(AP.Project.Test).should_include('SubPackage');
+    },
+    
+    'should have lambda function' : function () {
+        value_of(AP.lambda).should_not_be_undefined();
+        value_of(AP.lambda).should_not_be_null();
+        value_of(typeof AP.lambda).should_be('function');
+    },
+    
+    'if a function is passed in lambda, function should be returned': function(){
+		var fn = function(a,b){ return a; };
+		value_of(AP.lambda(fn)).should_be(fn);
+	},
+
+	'lambda should return a function that returns the value passed when called': function(){
+		value_of(AP.lambda('hello world!')()).should_be('hello world!');
+	},
+	
+	'should have log method' : function () {
+	    value_of(AP.log).should_not_be_undefined();
+        value_of(AP.log).should_not_be_null();
+        value_of(typeof AP.log).should_be('function');
+	},
+	
+	'should have generateUID method' : function () {
+	    value_of(AP.generateUID).should_not_be_undefined();
+        value_of(AP.generateUID).should_not_be_null();
+        value_of(typeof AP.generateUID).should_be('function');
+	},
+	
+	'generateUID should return unique value based on incremental uidIndex value' : function () {
+	    var uids = [], i = 5;
+	    while(i--) {
+	        uids.push(AP.generateUID());
+	    }
+	    value_of(uids).should_have(5, 'items');
+	    for (i = 0; i < 5; i++) {
+	        value_of(uids).should_include('ap-' + i);
+	    }	    
+	},
+	
+	'generateUID can take custom prefix argument and return new UID based on it' : function () {
+	    value_of(AP.generateUID('customPrefix')).should_be('customPrefix-5');
+	},
+	
+	'should have stamp method' : function () {
+	    value_of(AP.stamp).should_not_be_undefined();
+        value_of(AP.stamp).should_not_be_null();
+        value_of(typeof AP.stamp).should_be('function');
+	},
+	
+	'stamp should create _uid property on the passed object, if it has no one yet' : function () {
+	    var obj = {};
+	    AP.stamp(obj);
+	    value_of(obj).should_include('_uid');
+	    value_of(obj._uid).should_be('ap-6');
+	    AP.stamp(obj);
+	    value_of(obj).should_include('_uid');
+	    value_of(obj._uid).should_be('ap-6');
+	},
+	
+	'should have add method' : function () {
+	    value_of(AP.add).should_not_be_undefined();
+        value_of(AP.add).should_not_be_null();
+        value_of(typeof AP.add).should_be('function');
+	},
+	
+	'add method should add module name into AP.config.envinronment.modules object' : function () {
+	    AP.add('briteModule', function () {}, '0.0.1');
+	    value_of(AP.config.envinronment.modules).should_include('briteModule');
+	    value_of(typeof AP.config.envinronment.modules.briteModule).should_be('object');
+	},
+	
+	'add method should expand string version into version object' : function () {
+	    var m = AP.config.envinronment.modules;
+	    AP.add('anotherOneModule', function () {}, '1.2.3');
+	    value_of(typeof m.anotherOneModule).should_be('object');
+	    value_of(m.anotherOneModule).should_include('version');
+	    value_of(m.anotherOneModule.version).should_include('major');
+	    value_of(m.anotherOneModule.version.major).should_be('1');
+	    value_of(m.anotherOneModule.version).should_include('minor');
+	    value_of(m.anotherOneModule.version.minor).should_be('2');
+	    value_of(m.anotherOneModule.version).should_include('micro');
+	    value_of(m.anotherOneModule.version.micro).should_be('3');
+	},
+	
+	'add method should check requirements and will throw error if there is no present required module name' : function () {
+	    var m = AP.config.envinronment.modules;
+	    try {
+	        AP.add('secondModule', function () {}, '1.2.3', [{ name : 'firstModule' }]);
+	    } catch (error) {
+	        value_of(error.message)
+	            .should_be('Module registration failure: module secondModule requires module firstModule');
+	    }
+	},
+	
+	'add method should check requirements and will throw error if specified version is less then present' : function () {
+	    AP.add('firstModule', function () {}, '0.0.1');
+	    try {
+	        AP.add('secondModule', function () {}, '1.2.3', [{ name : 'firstModule', minVersion : '1.3.3' }]);
+	    } catch (error) {
+	        value_of(error.message)
+	            .should_be('Module registration failure: module secondModule requires module firstModule version at least 1.3.3');
+	    }
+	},
+	
+	'add method should check requirements and will throw error if specified version is more then present' : function () {
+	    AP.add('thirdModule', function () {}, '2.0.1');
+	    try {
+	        AP.add('fourthModule', function () {}, '1.2.3', [{ name : 'thirdModule', maxVersion : '1.3.3' }]);
+	    } catch (error) {
+	        value_of(error.message)
+	            .should_be('Module registration failure: module fourthModule requires module thirdModule version no greater than 1.3.3');
+	    }
+	},
+	
+	'add method should check requirements and will differentiate major, minor, micro number in version requirement' : function () {
+	    var m = AP.config.envinronment.modules;
+	    AP.add('fifthModule', function () {}, '1.2.3');
+	    value_of(m).should_include('fifthModule');
+	    
+	    AP.add('sixsModule', function () {}, '1.2.3', [{ name : 'fifthModule', minVersion : '1' }]);
+	    value_of(m).should_include('sixsModule');
+	    AP.add('sevensModule', function () {}, '1.2.3', [{ name : 'fifthModule', minVersion : '1.2' }]);
+	    value_of(m).should_include('sevensModule');
+	    
+	    try {
+	        AP.add('eightsModule', function () {}, '1.2.3', [{ name : 'fifthModule', minVersion : '2' }]);
+	    } catch (error) {
+	        value_of(error.message)
+	            .should_be('Module registration failure: module eightsModule requires module fifthModule version at least 2');
+	    }
+	    
+	    AP.add('ninesModule', function () {}, '1.2.3', [{ name : 'fifthModule', maxVersion : '1' }]);
+	    value_of(m).should_include('ninesModule');
+	    AP.add('tensModule', function () {}, '1.2.3', [{ name : 'fifthModule', maxVersion : '1.2' }]);
+	    value_of(m).should_include('tensModule');
+	    
+	    try {
+	        AP.add('elevensModule', function () {}, '1.2.3', [{ name : 'fifthModule', maxVersion : '1.1' }]);
+	    } catch (error) {
+	        value_of(error.message)
+	            .should_be('Module registration failure: module elevensModule requires module fifthModule version no greater than 1.1');
+	    }
+	},
+	
+	'add method should do nothing if module with present name already exist with same or greater version' : function () {
+	    var m = AP.config.envinronment.modules;
+	    AP.add('moduleWithSameName', function () {}, '1.2.3');
+	    value_of(m).should_include('moduleWithSameName');
+	    value_of(m.moduleWithSameName.version).should_be({major: 1, minor: 2, micro: 3});
+	    
+	    AP.add('moduleWithSameName', function () {}, '1.2.3');
+	    value_of(m).should_include('moduleWithSameName');
+	    value_of(m.moduleWithSameName.version).should_be({major: 1, minor: 2, micro: 3});
+	    
+	    AP.add('moduleWithSameName', function () {}, '0.0.1');
+	    value_of(m).should_include('moduleWithSameName');
+	    value_of(m.moduleWithSameName.version).should_be({major: 1, minor: 2, micro: 3});
+	},
+	
+	'add method should register module with same name, if provided version is more than existing' : function () {
+	    var m = AP.config.envinronment.modules;
+	    AP.add('moduleWithSameName', function () {}, '1.2.3');
+	    value_of(m).should_include('moduleWithSameName');
+	    value_of(m.moduleWithSameName.version).should_be({major: 1, minor: 2, micro: 3});
+	    
+	    AP.add('moduleWithSameName', function () {}, '2.13.256');
+	    value_of(m).should_include('moduleWithSameName');
+	    value_of(m.moduleWithSameName.version).should_be({major: 2, minor: 13, micro: 256});
+	},
+	
+	'add method should pass AP instance into module binding function' : function () {
+	    value_of(AP.WOW).should_be_undefined();
+	    AP.add('wow', function (A) { A.WOW = A.WOW || {}; }, '1.2.3');
+	    value_of(typeof AP.WOW).should_be('object');
+	},
+	
+	'should have check method' : function () {
+	    value_of(AP.check).should_not_be_undefined();
+        value_of(AP.check).should_not_be_null();
+        value_of(typeof AP.check).should_be('function');
+	},
+	
+	'check should return true, if object exist, otherwise, return true' : function () {
+	    var abracadabra;
+	    value_of(AP.check(abracadabra)).should_be_false();
+        value_of(AP.check(null)).should_be_false();
+        value_of(AP.check('')).should_be_false();
+        value_of(AP.check(0)).should_be_false();
+        value_of(AP.check(1)).should_be_true();
+        value_of(AP.check('a')).should_be_true();
+        value_of(AP.check([])).should_be_true();
+        value_of(AP.check({})).should_be_true();
+        value_of(AP.check(new Date())).should_be_true();
+	}
+});
---
+++ src/core/core.js
@@ -0,0 +1,252 @@
+/**
+ * AP Core
+ * @module ap
+ * @author Alexander <xan> Ulizko
+ */
+(function () {
+    if (typeof AP === 'undefined' || !AP) {
+        /**
+         * The AP global namespace object. If AP is already defined,
+         * existing AP object will not be overwritten so that defined
+         * namespaces are preserved.
+         * @class AP
+         * @constructor
+         * @param o Optional configuration object.  Options:
+         * <ul>
+         *  <li>------------------------------------------------------------------------</li>
+         *  <li>Global:</li>
+         *  <li>------------------------------------------------------------------------</li>
+         *  <li>debug:
+         *  Turn debug statements on or off</li>
+         *  <li>win:
+         *  The target window/frame</li>
+         * </ul>
+         */
+        AP = function (o) {
+            var A = this;
+            // Allow var ap = AP() instead of var ap = new AP()
+            if (A == window) {
+                return new AP(o);
+            } else {
+                // set up the core environment
+                A._init(o);
+                // A._setup();
+                return A;
+            }
+        };
+    }
+    
+    AP.prototype = {
+        /**
+         * Initialize this AP instance
+         * @param o config options. For details, @see <AP>
+         * @private
+         */
+        init : function (o) {
+            o = o || {};
+
+            // find targeted window
+            var w = (o.win) ? (o.win.contentWindow) : o.win || window;
+            o.win = w;
+            o.doc = w.document;
+            o.envinronment = {
+                prefix : 'ap',
+                uidIndex : 0,
+                modules : {}
+            };
+            
+            this.config = o;
+            this.constructor = AP;
+        },
+        /**
+         * Returns the namespace specified and creates it if it doesn't exist
+         * <pre>
+         * AP.namespace("property.package");
+         * AP.namespace("AP.property.package");
+         * </pre>
+         * Either of the above would create AP.property, then
+         * AP.Project.property.package
+         *
+         * Be careful when naming packages. Reserved words may work in some browsers
+         * and not others. For instance, the following will fail in Safari:
+         * <pre>
+         * AP.namespace("really.long.nested.namespace");
+         * </pre>
+         * This fails because "long" is a future reserved word in ECMAScript
+         *
+         * @method namespace
+         * @param  {string*} arguments 1-n namespaces to create
+         * @return {object}  A reference to the last namespace object created
+         */
+        namespace: function() {
+            var a=arguments, o=null, i, j, d;
+            for (i=0; i<a.length; i=i+1) {
+                d = a[i].split(".");
+                
+                o = this.Project ? this.Project : this.Project = {};
+                for (j = 0; j < d.length; j = j + 1) {
+                    o[d[j]] = o[d[j]] || {};
+                    o = o[d[j]];
+                }
+            }
+            return o;
+        },
+        
+        /**
+         * Creates an empty function which does nothing but return the value passed.
+         * Inspired with MooTools $lambda function
+         * implementation was taken from MooTools.1.2.1 Core.js, line 201
+         * @method lambda
+         * @param {Mixed} value The value for the created function to return.
+         * @return {Function} A function which returns the desired value.
+         */
+        lambda : function (value) {
+        	return (typeof value == 'function') ? value : function(){
+        		return value;
+        	};
+        },
+        
+        /**
+         * Checks to see if a value exists
+         * Inspired with MooTools $chk method
+         * @method check
+         * @param {Mixed} o the item to inspect
+         * @return {Boolean} If the object passed in exists, return true. Otherwise, returns false
+         */
+        check : function (o) {
+            return !!o;
+        },
+        
+        
+        // stub which will be misplaced by Console module
+        // inspired with YUI log function
+        log : function () {},
+        
+        /**
+         * Generate an id that is unique among all AP instances
+         * inspired by YUI 3.0 <guid> method.
+         * @method generateUID
+         * @return {String} the guid
+         */
+        generateUID : function (pre) {
+            var e = this.config.envinronment, p = (pre) || e.prefix;
+            return p +'-' + e.uidIndex++;
+        },
+        
+        /**
+         * Stamps an object with a guid.  If the object already
+         * has one, a new one is not created
+         * Implementation was taken from YUI 3.0.0.pr1 line 448
+         * @method stamp
+         * @param o The object to stamp
+         * @return {String} The object's unique identifier
+         */
+        stamp: function(o) {
+
+            if (!o) {
+                return o;
+            }
+
+            var uid = (typeof o === 'string') ? o : o._uid;
+
+            if (!uid) {
+                uid = this.generateUID();
+                o._uid = uid;
+            }
+
+            return uid;
+        },
+        
+        /**
+         * Register a module
+         * Inspired from YUI.3.0.0.pr1 add module
+         * @method add
+         * @param name {String} module name
+         * @param fn {Function} entry point into the module that
+         * is used to bind module to the AP instance
+         * @param version {String} version string
+         * @return {AP} the AP instance
+         */
+        add : function (name, fn, version, requirements) {
+            var m = this.config.envinronment.modules, requirement, i, oldInstance, isExistingModuleOutOfDate = true,
+            // convert version string into object
+            v = {
+                version : (function (version) {
+                    var v = {
+                        major : 0,
+                        minor : 0,
+                        micro : 0
+                    };
+                    if (typeof version === 'string') {
+                        version.replace(/(\d+)\.(\d+)\.(\d+)/, function (m, major, minor, micro) {
+                            v.major = major;
+                            v.minor = minor;
+                            v.micro = micro;
+                        });
+                    }
+                    return v;
+                })(version)
+            };
+            
+            if (typeof (oldInstance = m[name]) === 'object') { 
+                // we already register module with same name
+                // time to compare versions 
+                if (!((v.version.major > oldInstance.version.major) ||
+                    (v.version.major == oldInstance.version.major && v.version.minor > oldInstance.version.minor) ||
+                    (v.version.major == oldInstance.version.major && v.version.minor > oldInstance.version.minor && v.version.micro > oldInstance.version.micro))) {
+                    
+                    return;
+                } 
+            }
+            
+            // check requirements
+            if (typeof requirements !== 'undefined' && requirements) {
+                i = 0;
+                while(requirement = requirements[i++]) {
+                    var requirementName = requirement.name, 
+                        error = 'Module registration failure: module ' + name + ' requires module ' + requirementName;
+                        
+                    if (typeof requirementName === 'string' && typeof m[requirementName] === 'undefined') {
+                        throw new Error(error);
+                    }
+                    
+                    if (typeof requirement.minVersion === 'string') {
+                        // parse requirement into major, minor, micro numbers
+                        var version = requirement.minVersion.split(/\./), j = 0, existedVersion = m[requirementName].version;
+                        if (existedVersion.major < version[0] || existedVersion.minor < version[1] || 
+                              existedVersion.micro < version[2]) {
+                            throw new Error(error + ' version at least ' + requirement.minVersion);
+                        }
+                    }
+                    
+                    if (typeof requirement.maxVersion === 'string') {
+                        // parse requirement into major, minor, micro numbers
+                        var version = requirement.maxVersion.split(/\./), j = 0, existedVersion = m[requirementName].version;
+                        if (existedVersion.major > version[0] || existedVersion.minor > version[1] || 
+                              existedVersion.micro > version[2]) {
+                            throw new Error(error + ' version no greater than ' + requirement.maxVersion);
+                        }
+                    }
+                }
+            }
+            
+            
+            // expand version into major, minor, micro numbers (inspired from Yahoo! BrowserPlus service version system)
+            m[name] = v;
+            
+            fn(this);
+        }
+    };
+    
+    var A = AP, p = A.prototype, i;
+
+    // inheritance utilities are not available yet
+    for (i in p) {
+        if (true) { // hasOwnProperty not available yet and not needed
+           A[i] = p[i];
+        }
+    }
+    
+    A.init();
+    
+})();
